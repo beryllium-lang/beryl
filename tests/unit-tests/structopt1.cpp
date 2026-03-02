c3737cd3c3f124e0c9ba408e81738f91fad1b756
Aneesh Abbineni <aabbineni@all.health>
2026-03-02 18:28:19 -0500

    more complicated struct test

--- tests/unit-tests/structopt1.cpp
+++ tests/unit-tests/structopt1.cpp
@@ -1,27 +1,19 @@
 #include "struct-opt/struct_opt.hpp"
+#include <gtest/gtest.h>
 #include <iostream>
 
-int main() {
-  std::vector<beryl::Field> fields = {
-    {4, 4},
-    {3, 1},
-    {3, 1},
-    {1, 1},
-    {1, 1},
-    {1, 1},
-    {1, 1},
-    {1, 1}
-  };
+TEST(StructOpt, Test1) {
+  std::vector<beryl::Field> fields = {{8, 8}, {3, 4}, {3, 4}, {1, 1},
+                                      {1, 1}, {4, 4}, {2, 2}};
 
   beryl::opt_layout(fields);
 
   std::cout << "Optimized field order:\n";
-  for (const auto& field : fields) {
-    std::cout << "Size: " << field.size << ", Alignment: " << field.alignment << "\n";
+  for (const auto &field : fields) {
+    std::cout << "Size: " << field.size << ", Alignment: " << field.alignment
+              << "\n";
   }
 
   auto [total_size, struct_alignment] = beryl::compute_struct_size(fields);
-  std::cout << "Total struct size: " << total_size << ", Struct alignment: " << struct_alignment << "\n";
-
-  return 0;
+  EXPECT_EQ(total_size, 24);
 }

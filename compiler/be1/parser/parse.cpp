bf6507d982543976d209a72aa7badf8d33f8d14a
Aneesh Abbineni <aabbineni@all.health>
2026-03-03 22:09:29 -0500

    Made some updates

--- compiler/be1/parser/parse.cpp
+++ compiler/be1/parser/parse.cpp
@@ -9,8 +9,7 @@ namespace beryl::be1 {
       auto class_ = arena.alloc<ast::ClassDecl>();
       tokens.advance();
       if (auto tok = tokens.peek(1); tok != Token::IDENT) beryl::throw_lex_error("class does not have identifier", tok.line, tok.col);
-      else
-        class_->name = tok.metadata;
+      else class_->name = tok.metadata;
       tokens.advance();
       if (auto tok = tokens.peek(1); tok != Token::OPEN_CURLY) beryl::throw_lex_error("class not opened with brace", tok.line, tok.col);
       tokens.advance();
@@ -34,6 +33,7 @@ namespace beryl::be1 {
 
     ast::ImportDecl* construct_import(TokenStream& tokens, Arena& arena) {
       auto import = arena.alloc<ast::ImportDecl>();
+      import->module_name = "";
       if (tokens.peek() == Token::PUBLISH) {
         import->publish = true;
         tokens.advance();
@@ -41,6 +41,22 @@ namespace beryl::be1 {
       auto next_token = tokens.peek(1);
       if (next_token != Token::IDENT && next_token != Token::DOT) {
         throw_lex_error("a module name must come after an import statement", next_token.line, next_token.col);
+      }
+      tokens.advance();
+      bool prev_is_dot = next_token == Token::DOT;
+      if (prev_is_dot) {
+        import->module_name += '.';
+        tokens.advance();
+      }
+      while (true) {
+        const auto& curr_tok = tokens.peek();
+        if (const auto& tok = tokens.peek(1); tok != Token::SEMI && tok != Token::FROM)
+          throw_lex_error("invalid import statement", tok.line, tok.col);
+        if ((curr_tok == Token::DOT && prev_is_dot) || (curr_tok == Token::IDENT && !prev_is_dot))
+          throw_lex_error("invalid module name", curr_tok.line, curr_tok.col);
+        if (curr_tok == Token::DOT) {
+        }
+        if (curr_tok == Token::IDENT) import->module_name += curr_tok.metadata;
       }
       tokens.advance();
       return import;
@@ -63,18 +79,14 @@ namespace beryl::be1 {
       if (tokens.peek() == Token::CLASS) tree->body.emplace_back(construct_class(tokens, arena));
       else if (tokens.peek() == Token::FUNC || (tokens.peek() == Token::PUBLISH && tokens.peek(1) == Token::FUNC))
         tree->body.emplace_back(construct_func(tokens, arena));
-      else if (tokens.peek() == Token::NMSP)
-        tree->body.emplace_back(construct_nsp(tokens, arena));
+      else if (tokens.peek() == Token::NMSP) tree->body.emplace_back(construct_nsp(tokens, arena));
       else if (tokens.peek() == Token::VAR || (tokens.peek() == Token::PUBLISH && tokens.peek(1) == Token::VAR))
         tree->body.emplace_back(construct_var(tokens, arena));
-      else if (tokens.peek() == Token::IMPORT || (tokens.peek() == Token::PUBLISH && tokens.peek(1) == Token::IMPORT)) 
+      else if (tokens.peek() == Token::IMPORT || (tokens.peek() == Token::PUBLISH && tokens.peek(1) == Token::IMPORT))
         tree->body.emplace_back(construct_import(tokens, arena));
-      else if (tokens.peek() == Token::ENUM)
-        tree->body.emplace_back(construct_enum(tokens, arena));
-      else if (tokens.peek() == Token::TRAIT)
-        tree->body.emplace_back(construct_trait(tokens, arena));
-      else
-        beryl::throw_lex_error("unknown global-level declaration", tokens.peek().line, tokens.peek().col);
+      else if (tokens.peek() == Token::ENUM) tree->body.emplace_back(construct_enum(tokens, arena));
+      else if (tokens.peek() == Token::TRAIT) tree->body.emplace_back(construct_trait(tokens, arena));
+      else beryl::throw_lex_error("unknown global-level declaration", tokens.peek().line, tokens.peek().col);
       continue;
     }
     return tree;

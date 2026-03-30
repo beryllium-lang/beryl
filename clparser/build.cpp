d1f627d561d95cc13073811c9a480f509834e714
PentagonX <pentagonx@github.com>
2026-03-29 21:10:57 -0400

    increased memory allocation size

--- clparser/build.cpp
+++ clparser/build.cpp
@@ -17,7 +17,7 @@ namespace fs = std::filesystem;
 
 namespace beryl {
   void build(BuildArgs args) {
-    beryl::Arena alloc(128 * 1024 * 2048);
+    beryl::Arena alloc(128 * 1024 * 2048); //increased memory
 
     llvm::LLVMContext context;
 
